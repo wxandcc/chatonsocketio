78b9fb4940debaef34cd6ee8080d775e3ce8ca09
weixing <weixing@jovision.com>
2015-11-26 15:47:00 +0800

    fix bug

--- model/orm/friends.js
+++ model/orm/friends.js
@@ -9,6 +9,10 @@ var conn = require("./connectPool.js").mysqlConnPool;
 module.exports ={
     //call back with array of friends {id,username,avatar_file}
     friends: function(uid,callback){
+                var uid = parseInt(uid,10);
+                if(isNaN(uid)){
+                    callback(null,[]);
+                }
                 conn.query('select user.id,user.username from gr_users as user inner join gr_user_follow ' +
                     'as follow on follow.fid = user.id where follow.uid='+parseInt(uid, 10),
                     function(err,friends){
